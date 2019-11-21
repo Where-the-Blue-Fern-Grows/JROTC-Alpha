3de3978376aa121c3111135bfbcbc5df1a6a92d8
Abiy Ahmed Ali <58038133+abiy-ahmed@users.noreply.github.com>
2019-11-21 10:27:19 -0600

    Create 1.sh

---
+++ Ubuntu14/1.sh
@@ -0,0 +1,37 @@
+sudo fuser -k //var/lib/dpkg/lock-frontend
+sudo apt-get purge -y wireshark
+sudo apt-get purge -y apache2
+sudo apt-get purge -y apache2-bin
+sudo apt-get purge -y apache2-data
+sudo apt-get purge -y wireshark-common
+sudo apt-get purge -y totem
+sudo apt-get purge -y cheese
+sudo apt-get purge -y compiz
+sudo apt-get purge -y gnomine
+sudo apt-get purge -y nmap
+sudo apt-get purge -y mahjongg
+sudo apt-get purge -y rhythmbox
+sudo apt-get purge -y shotwell
+sudo apt-get purge -y medusa
+sudo apt-get purge -y john
+sudo apt-get purge -y sqlmap
+sudo apt-get purge -y hydra
+sudo apt-get purge -y zenmap
+sudo apt-get purge -y ophcrack
+sudo apt-get purge -y minetest
+sudo apt-get purge -y samba
+sudo apt-get purge -y samba-common
+sudo apt-get purge -y telnet
+sudo apt-get purge -y tcpdump
+sudo apt-get purge -y cups
+sudo apt-get purge -y postgresql
+sudo apt-get purge -y libwireshark-data
+sudo apt-get purge -y libwireshark3
+sudo apt-get purge -y libwiretap3
+sudo apt-get purge -y musescore-soundfont-gm
+Sudo apt-get purge -y postgresql\*
+sudo apt-get install -y clamav clamav-daemon rkhunter chkrootkit auditd aide aide-common unattended-upgrades
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
