79044ba5e9fe1ce10353b0b9811fe86e8e731f1c
Abiy Ahmed Ali <58038133+abiy-ahmed@users.noreply.github.com>
2019-11-25 11:05:35 -0600

    Update firewall.ps1

--- Win10/firewall.ps1
+++ Win10/firewall.ps1
@@ -1,5 +1,14 @@
 #Enable the entire firewall
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
+Set-NetFirewallProfile -Name Domain,Public,Private -Enabled True
 
 #Set direction defaults
 Set-NetFirewallProfile -DefaultInboundAction Block -DefaultOutboundAction Allow
+
+#Settings for profiles
+Set-NetFirewallProfile -Name Domain,Private,Public -NotifyOnListen False
+
+#Unique Settings for Public
+Set-NetFirewallProfile -Name Public -AllowLocaFirewallRules False -AllowLocalIPsecRules False
+
+#Logging for Domain, Private, and Public
+Set-NetFirewallProfile -Name Domain,Private,Public -LogFileName %SystemRoot%\System32\LogFiles\Firewall\pfirewall.log -LogMaxSizeKilobytes 16384 -LogBlocked True -LogAllowed True
