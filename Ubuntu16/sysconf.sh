33246001c5863d8dfd3467b80d1f4b3bd889632e
Abiy Ahmed Ali <58038133+abiy-ahmed@users.noreply.github.com>
2019-11-27 14:17:34 -0600

    Update sysconf.sh

--- Ubuntu16/sysconf.sh
+++ Ubuntu16/sysconf.sh
@@ -6,9 +6,6 @@ rmmod hfs
 rmmod hfsplus
 rmmod udf
 
-#no idea what this does yet
-df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d \( -perm -0002 -a ! -perm -1000 \) 2>/dev/null
-
 #nor this
 systemctl is-enabled autofs
 
@@ -19,11 +16,6 @@ aideinit
 sudo chmod 702 //etc/pam.d/common-auth
 echo "auth required pam_tally2.so file=/var/log/tallylog deny=5 even_deny_root\ unlock_time=900" >> //etc/pam.d/common-auth
 sudo chmod 700 //etc/pam.d/common-auth
-
-#configure password complexity requirements
-sudo chmod 702 //etc/pam.d/common-password
-sudo sed -i '/obscure sha512/aremember=5\nminlen=15\nucredit=-1\nlcredit=-1\ndcredit=-1\nocredit=-'//etc/pam.d/common-password
-sudo chmod 700 //etc/pam.d/common-password
 
 #enable auditing
 sudo auditctl -e 1
@@ -91,12 +83,3 @@ sudo chmod 702 //etc/host.conf
 echo "order bind,hosts" >> //etc/host.conf
 echo "nospoof on" >> //etc/host.conf
 sudo chmod 700 //etc/host.conf
-
-
-
-
-
-
-
-
-
