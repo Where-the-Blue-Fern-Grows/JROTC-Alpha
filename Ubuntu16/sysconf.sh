7f23f688996786436b850014d10ba68267c37b97
abiy-ahmed <cybercom276@gmail.com>
2019-11-27 12:46:49 -0600

    prepare sysconf.sh

--- Ubuntu16/sysconf.sh
+++ Ubuntu16/sysconf.sh
@@ -17,11 +17,19 @@ aideinit
 
 #configure common-auth
 sudo chmod 702 //etc/pam.d/common-auth
-echo auth required pam_tally2.so file=/var/log/tallylog deny=5 even_deny_root\ unlock_time=900 >> //etc/pam.d/common-auth
+echo "auth required pam_tally2.so file=/var/log/tallylog deny=5 even_deny_root\ unlock_time=900" >> //etc/pam.d/common-auth
 sudo chmod 700 //etc/pam.d/common-auth
 
 #configure password complexity requirements
 sudo chmod 702 //etc/pam.d/common-password
 sudo sed -i '/obscure sha512/aremember=5\nminlen=15\nucredit=-1\nlcredit=-1\ndcredit=-1\nocredit=-'//etc/pam.d/common-password
 sudo chmod 700 //etc/pam.d/common-password
+
+#enable auditing
+sudo auditctl -e 1
+
+#dunno what this does
+sudo sysctl -p
+
+#configure sysctl.conf
 
