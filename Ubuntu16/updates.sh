dba063a7f0ae9dd467775efa65bbc10120378022
Abiy Ahmed Ali <58038133+abiy-ahmed@users.noreply.github.com>
2019-11-27 12:05:13 -0600

    Update updates.sh

--- Ubuntu16/updates.sh
+++ Ubuntu16/updates.sh
@@ -6,7 +6,6 @@ sudo apt-get purge -y apache2-data
 sudo apt-get purge -y wireshark-common
 sudo apt-get purge -y totem
 sudo apt-get purge -y cheese
-sudo apt-get purge -y compiz
 sudo apt-get purge -y gnomine
 sudo apt-get purge -y nmap
 sudo apt-get purge -y mahjongg
@@ -21,9 +20,12 @@ sudo apt-get purge -y ophcrack
 sudo apt-get purge -y minetest
 sudo apt-get purge -y samba
 sudo apt-get purge -y samba-common
+sudo apt-get purge -y python-samba
 sudo apt-get purge -y telnet
 sudo apt-get purge -y tcpdump
 sudo apt-get purge -y cups
+sudo apt-get purge -y cups-browsed
+sudo apt-get purge -y cups-common
 sudo apt-get purge -y postgresql
 sudo apt-get purge -y libwire*
 sudo apt-get purge -y musescore-soundfont-gm
@@ -32,4 +34,6 @@ sudo apt-get update -y
 sudo apt-get install -y clamav clamav-daemon rkhunter chkrootkit auditd aide aide-common unattended-upgrades thunderbird tree
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
+sudo apt-get upgrade -y
+sudo apt-get update -y
 sudo apt-get upgrade -y
