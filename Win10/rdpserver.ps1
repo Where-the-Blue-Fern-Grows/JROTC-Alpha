dca066540b2f6b27e7aade5c206bf02d968e163d
Abiy Ahmed Ali <58038133+abiy-ahmed@users.noreply.github.com>
2019-11-29 20:32:15 -0600

    Update rdpserver.ps1

--- Win10/rdpserver.ps1
+++ Win10/rdpserver.ps1
@@ -1,5 +1,8 @@
 #Turn off preset RDP firewall rules
+Set-NetFirewallRule -DisplayName "*Remote Desktop*" -Enabled True -Action Allow -Profile Any
 
 #Allow RDP in firewall in+out
+Set-NetFirewallRule -LocalPort 3389 -Enabled True -Action Allow
 
 #RDP gpedit.msc
+
