fd2e12e8fe0c188ec9e9768aab0cb6d0006741f1
Abiy Ahmed Ali <58038133+abiy-ahmed@users.noreply.github.com>
2019-12-04 09:55:37 -0600

    Update updates.sh

--- Ubuntu16/updates.sh
+++ Ubuntu16/updates.sh
@@ -1,4 +1,5 @@
 sudo fuser -k //var/lib/dpkg/lock-frontend
+prelink -ua
 sudo apt-get purge -y wireshark
 sudo apt-get purge -y apache2
 sudo apt-get purge -y apache2-bin
@@ -29,7 +30,14 @@ sudo apt-get purge -y cups-common
 sudo apt-get purge -y postgresql
 sudo apt-get purge -y libwire*
 sudo apt-get purge -y musescore-soundfont-gm
-Sudo apt-get purge -y postgresql\*
+sudo apt-get purge -y postgresql\*
+sudo apt-get purge -y prelink
+sudo apt-get purge -y openbsd-inetd
+sudo apt-get purge -y nis
+sudo apt-get purge -y rsh-client
+sudo apt-get purge -y rsh-redone-client
+sudo apt-get purge -y talk
+sudo apt-get purge -y ldap-utils
 sudo apt-get update -y
 sudo apt-get install -y clamav clamav-daemon rkhunter chkrootkit auditd aide aide-common unattended-upgrades thunderbird tree
 sudo apt-get autoremove -y
