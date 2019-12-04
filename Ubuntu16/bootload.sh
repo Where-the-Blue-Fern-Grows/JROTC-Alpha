ce501febc6055ba646cbe2c23bb141ffda234833
Abiy Ahmed Ali <58038133+abiy-ahmed@users.noreply.github.com>
2019-12-04 08:23:02 -0600

    Create bootload.sh

---
+++ Ubuntu16/bootload.sh
@@ -0,0 +1,13 @@
+echo -e "JROTC$$cp18alpha\nJROTC$$cp18alpha" | grub-mkpasswd-pbkdf2 > grub.txt
+sed -i 's/Enter password: //' grub.txt
+sed -i 's/Reenter password: //' grub.txt
+sed -i 's/PBKDF2 hash of your password is //' grub.txt
+paste -sd "" grub.txt > clip
+cat clip > grub.txt
+
+sudo chmod 777 //etc/grub.d/00_header
+echo "cat <<EOF" >> //etc/grub.d/00_header
+echo "set superusers="root"" >> //etc/grub.d/00_header
+echo "password pbkdf2 root " >> //etc/grub.d/00_header
+PASS=cat grub.txt
+sed -i '${s/$/$PASS/}' //etc/grub.d/00_header
