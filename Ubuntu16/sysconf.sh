236b04349a630f62c2f8d30690625acc7a6df941
Abiy Ahmed Ali <58038133+abiy-ahmed@users.noreply.github.com>
2019-12-04 23:36:25 -0600

    Update sysconf.sh

--- Ubuntu16/sysconf.sh
+++ Ubuntu16/sysconf.sh
@@ -198,6 +198,61 @@ echo "admin_space_left_action = halt" >> //etc/audit/auditd.conf
 echo "max_log_file_action = keep_logs" >> //etc/audit/auditd.conf
 sudo chmod 700 //etc/audit/auditd.conf
 systemctl reload auditd
+sudo chmod 777 //etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S adjtimex -S settimeofday -k time-change" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time- change" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S clock_settime -k time-change" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b32 -S clock_settime -k time-change -w /etc/localtime -p wa -k time-change" >> //etc/audit/audit.rules
+echo "-w /etc/group -p wa -k identity" >> //etc/audit/audit.rules
+echo "-w /etc/passwd -p wa -k identity" >> //etc/audit/audit.rules
+echo "-w /etc/gshadow -p wa -k identity" >> //etc/audit/audit.rules
+echo "-w /etc/shadow -p wa -k identity" >> //etc/audit/audit.rules
+echo "-w /etc/security/opasswd -p wa -k identity" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S sethostname -S setdomainname -k system-locale" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale" >> //etc/audit/audit.rules
+echo "-w /etc/issue -p wa -k system-locale" >> //etc/audit/audit.rules
+echo "-w /etc/issue.net -p wa -k system-locale" >> //etc/audit/audit.rules
+echo "-w /etc/hosts -p wa -k system-locale" >> //etc/audit/audit.rules
+echo "-w /etc/sysconfig/network -p wa -k system-locale" >> //etc/audit/audit.rules
+echo "-w /etc/apparmor/ -p wa -k MAC-policy" >> //etc/audit/audit.rules
+echo "-w /etc/apparmor.d/ -p wa -k MAC-policy" >> //etc/audit/audit.rules
+echo "-w /var/log/faillog -p wa -k logins" >> //etc/audit/audit.rules
+echo "-w /var/log/lastlog -p wa -k logins" >> //etc/audit/audit.rules
+echo "-w /var/log/tallylog -p wa -k logins" >> //etc/audit/audit.rules
+echo "-w /var/run/utmp -p wa -k session" >> //etc/audit/audit.rules
+echo "-w /var/log/wtmp -p wa -k logins" >> //etc/audit/audit.rules
+echo "-w /var/log/btmp -p wa -k logins" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete" >> //etc/audit/audit.rules
+echo "-w /etc/sudoers -p wa -k scope" >> //etc/audit/audit.rules
+echo "-w /etc/sudoers.d/ -p wa -k scope" >> //etc/audit/audit.rules
+echo "-w /var/log/sudo.log -p wa -k actions" >> //etc/audit/audit.rules
+echo "-w /sbin/insmod -p x -k modules" >> //etc/audit/audit.rules
+echo "-w /sbin/rmmod -p x -k modules" >> //etc/audit/audit.rules
+echo "-w /sbin/modprobe -p x -k modules" >> //etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S init_module -S delete_module -k modules" >> //etc/audit/audit.rules
+sudo chmod 700 //etc/audit/auditd.conf
+
+sudo chmod 777 //etc/audit/.rules
+echo "-e 2" >> //etc/audit/.rules
+sudo chmod 700 //etc/audit/.rules
+
+
+
+
+
 
 #disable IPv6
 sudo chmod 777 //etc/default/grub
