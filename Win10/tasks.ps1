140576ef3b13c6257a399b539d04bfd8aa5cc661
Abiy Ahmed Ali <58038133+abiy-ahmed@users.noreply.github.com>
2020-01-14 08:29:14 -0600

    Update tasks.ps1

--- Win10/tasks.ps1
+++ Win10/tasks.ps1
@@ -1,3 +1,33 @@
 To-do:
 Bluetooth
 Downloaded Maps Manager
+Fax
+Geolocation Service
+Infrared
+Internet Connection Sharing (ICS)
+Link-Layer Topology Discovery Mapper
+Microsoft iSCSI Initiator Service
+Peer Networking
+Peer Name Resolution Protocol
+Plug and Play
+Phone Service
+PNRP Machine Name Publication Service
+Problems Reports and Solutions
+Remote Access Auto Connection Manager
+Remote Desktop Configuration
+Remote Desktop Services
+Remote Desktop Serivces UserMode
+Hyper-V Remote Desktop
+Remote Procedure Call (RPC) Locator
+Remote Registry
+Remote Access
+SNMP Trap
+SSDP Discovery
+UPnP Device Host
+Windows Error Reporting Service
+Windows Event Collector
+Windows Media Player Network Sharing
+Windows Mobile Hotspot Service
+Windows Push Notifications System
+Windows Remote Management
+Xbox
