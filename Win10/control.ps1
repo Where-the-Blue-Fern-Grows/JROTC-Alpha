6d8decaa682637689e81e7c404c2fd582595c721
Abiy Ahmed Ali <58038133+abiy-ahmed@users.noreply.github.com>
2020-01-16 08:23:10 -0600

    Create control.ps1

---
+++ Win10/control.ps1
@@ -0,0 +1,41 @@
+#Autoplay
+
+
+#BitLocker
+
+
+#Credential Manager
+
+
+#Device Manager
+
+
+#Devices and Printers
+Remove-Printer -Name *
+
+#File Explorer Options
+
+
+#Flash Player
+
+
+#Infrared
+
+
+#Language
+
+
+#Network and Sharing Center
+
+
+#Phone and Modem
+
+
+#Recovery
+
+
+#Security and Maintenance
+
+
+#Speech Recognition
+
