e16307f1d8b426efe7a0b0f2d2dc344dc142bbe2
Abiy Ahmed Ali <58038133+abiy-ahmed@users.noreply.github.com>
2020-01-24 07:54:07 -0600

    Update control.ps1

--- Win10/control.ps1
+++ Win10/control.ps1
@@ -1,23 +1,11 @@
 #AutoPlay
 #.bat for JROTC-Alpha/Win10/Registry/AutoPlay.reg file, but changed for HKU\S-1-5-21-XXXXXXXXX-XXXXXXXXXX-XXXXXXXXXX-XXXX (automate somehow)
 
-#BitLocker
-
-
-#Credential Manager
-
-
-#Device Manager
-
-
 #Devices and Printers
 Remove-Printer -Name *
 
 #File Explorer Options
 #.bat for JROTC-Alpha/Win10/Registry/Explorer.reg file, but changed for HKU\S-1-5-21-XXXXXXXXX-XXXXXXXXXX-XXXXXXXXXX-XXXX\Software\Microsoft\Windows\CurrentVersion\Explorer (automate somehow)
-
-#Flash Player
-
 
 #Infrared
 
