91ff90a654447232ed3174dc61a2fdde1c76634f
Abiy Ahmed Ali <58038133+abiy-ahmed@users.noreply.github.com>
2021-10-14 08:27:18 -0500

    Update updates.sh

--- Linux/Ubuntu16/updates.sh
+++ Linux/Ubuntu16/updates.sh
@@ -1,49 +1,11 @@
 sudo fuser -k //var/lib/dpkg/lock-frontend
 prelink -ua
 sudo apt-get update -y
-sudo apt-get purge -y wireshark
-sudo apt-get purge -y apache2
-sudo apt-get purge -y apache2-bin
-sudo apt-get purge -y apache2-data
-sudo apt-get purge -y wireshark-common
-sudo apt-get purge -y totem
-sudo apt-get purge -y cheese
-sudo apt-get purge -y nmap
-sudo apt-get purge -y mahjongg
-sudo apt-get purge -y rhythmbox*
-sudo apt-get purge -y shotwell*
-sudo apt-get purge -y medusa
-sudo apt-get purge -y john
-sudo apt-get purge -y sqlmap
-sudo apt-get purge -y hydra
-sudo apt-get purge -y zenmap
-sudo apt-get purge -y ophcrack
-sudo apt-get purge -y minetest
-sudo apt-get purge -y samba
-sudo apt-get purge -y samba-common
-sudo apt-get purge -y python-samba
-sudo apt-get purge -y telnet
-sudo apt-get purge -y tcpdump
-sudo apt-get purge -y cups
-sudo apt-get purge -y cups-browsed
-sudo apt-get purge -y cups-common
-sudo apt-get purge -y postgresql
-sudo apt-get purge -y libwire*
-sudo apt-get purge -y musescore-soundfont-gm
-sudo apt-get purge -y postgresql\*
-sudo apt-get purge -y prelink
-sudo apt-get purge -y openbsd-inetd
-sudo apt-get purge -y nis
-sudo apt-get purge -y rsh-client
-sudo apt-get purge -y rsh-redone-client
-sudo apt-get purge -y talk
-sudo apt-get purge -y ldap-utils
-sudo apt-get update -y
+sudo apt-get purge -y wireshark*
+sudo apt-get purge -y apache2* totem cheese nmap mahjongg rhythmbox* shotwell* medusa john sqlmap hydra zenmap ophcrack minetest samba* python-samba telnet tcpdump cups* postgresql libwire* musescore-soundfont-gm postgresql\* prelink openbsd-inetd nis rsh-client rsh-redone-client talk ldap-utils
 sudo apt-get install -y clamav clamav-daemon rkhunter chkrootkit auditd aide aide-common unattended-upgrades thunderbird tree apparmor ntp tcpd iptables rsyslog apparmor-utils
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
-sudo apt-get upgrade -y
-sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo dpkg-reconfigure -plow unattended-upgrades
 sudo freshclam
